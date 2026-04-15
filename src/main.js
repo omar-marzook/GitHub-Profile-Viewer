a27bee6337cfefb8cd0c1bc37ecfc292d60fdeec
Omar Marzouq <omarmarzook92@gmail.com>
2026-04-15 13:39:01 +0200

    add filter by lang

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 const loader = document.getElementById('loading');
 const repoList = document.querySelector('.repo-list');
 const profileName = document.querySelector('#profile');
+const allReposData = [];
 
-async function fetchRepos(username) {
+async function fetchGitHubAPI(username) {
     loader.style.display = 'block';
     repoList.style.display = 'none';
 
@@ -19,7 +20,10 @@ async function fetchRepos(username) {
 
         const data = await response.json();
 
-        handleFetchData(data);
+        allReposData.push(...data);
+
+        handleData(allReposData);
+        addLangsToFilter(allReposData);
     } catch (error) {
         profileName.innerHTML = `<span style="color: red">${error.message}</span>`;
     } finally {
@@ -28,16 +32,19 @@ async function fetchRepos(username) {
     }
 }
 
-const handleFetchData = (data) => {
-    if (data != false) {
-        profileName.innerHTML = data[0].owner.login;
+const handleData = (data) => {
+    profileName.innerHTML = data[0].owner.login;
+    implementCards(data);
+};
 
-        data.forEach((repo) => {
-            const cardHTML = `
+// Handle DOM
+const implementCards = (data) => {
+    data.forEach((repo) => {
+        const cardHTML = `
                 <article class="repo-card">
                     <h3 class="repo-card__name">${repo.name}</h3>
                     <section class="repo-card__details">
-                    <p class="repo-card__date">created date: ${repo.created_at.toString().split('T')[0]}</p>
+                    <p class="repo-card__date">Updated date: ${repo.updated_at.toString().split('T')[0]}</p>
                     <p class="repo-card__stars">${repo.stargazers_count} 🌟 stars</p>
                     <p class="repo-card__private">${repo.visibility} visibility</p>
                     <p class="repo-card__lang">${repo.language || 'Not specified'}</p>
@@ -45,18 +52,18 @@ const handleFetchData = (data) => {
                     <a class="repo-card__btn" href="${repo.html_url}" target="_blank" rel="noopener noreferrer">View Repository</a>
                 </article>
             `;
-            repoList.insertAdjacentHTML('beforeend', cardHTML);
-        });
-    } else {
-        profileName.innerHTML =
-            '<span style="color: red">User not found!</span>';
-    }
+        repoList.insertAdjacentHTML('beforeend', cardHTML);
+    });
 };
 
-const usernameInput = document.querySelector('#username');
+const clearCardsList = () => {
+    repoList.innerHTML = '';
+};
+
+const userNameInput = document.querySelector('#username');
 const searchBtn = document.querySelector('#getRepos');
 
-usernameInput.addEventListener('keydown', (e) => {
+userNameInput.addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
         e.preventDefault();
         searchBtn.click();
@@ -65,6 +72,48 @@ usernameInput.addEventListener('keydown', (e) => {
 
 searchBtn.addEventListener('click', () => {
     repoList.innerHTML = '';
-    const username = usernameInput.value;
-    fetchRepos(username);
+    const username = userNameInput.value;
+    fetchGitHubAPI(username);
+});
+
+const getLanguages = (data) => {
+    const langsArray = data
+        .map((repo) => repo.language)
+        .filter((lang) => lang !== null);
+
+    const languages = new Set(langsArray);
+
+    return languages;
+};
+
+const langSelect = document.querySelector('#language-filter');
+
+const addLangsToFilter = (data) => {
+    const languages = getLanguages(data);
+
+    languages.forEach((lang) => {
+        const option = document.createElement('option');
+        option.value = lang;
+        option.textContent = lang;
+        langSelect.appendChild(option);
+    });
+};
+
+const filterLanguage = () => {
+    const selectedLang = langSelect.value;
+
+    const filteredRepos = allReposData.filter(
+        (repo) => repo.language === selectedLang,
+    );
+    clearCardsList();
+    implementCards(filteredRepos);
+};
+
+langSelect.addEventListener('change', () => {
+    if (langSelect.value !== 'all') {
+        filterLanguage();
+    } else {
+        clearCardsList();
+        implementCards(allReposData);
+    }
 });
